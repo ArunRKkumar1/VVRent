72bc2097cf969d60b8b1e9290b723fe22314ab35
arun <arunkharwar2003@gmail.com>
2024-10-08 10:58:05 +0530

    chan

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react()]
+  build: {
+    rollupOptions: {
+      input: 'index.html'  // Make sure the file path is correct
+    }
+  }
 })
