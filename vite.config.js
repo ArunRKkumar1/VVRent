0b2434baec18d6a0e11b5128b79875f79a846d92
arun <arunkharwar2003@gmail.com>
2024-10-09 00:04:00 +0530

    so

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   port: 3000,   // change to any port number
+   port: 80,   // change to any port number
     open: true,   // open the browser automatically
     host: '0.0.0.0',  // allow external access for development
   
