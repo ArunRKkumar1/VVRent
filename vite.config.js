62d2e4697db4bfa8b50e91060e12ee58b0a3f2dd
arun <arunkharwar2003@gmail.com>
2024-10-08 12:01:23 +0530

    1

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 export default defineConfig({
   server: {
-    port: 5173, // Change to a different port, like 3000
+    port:80, // Change to a different port, like 3000
     host: true,
   }
 });
